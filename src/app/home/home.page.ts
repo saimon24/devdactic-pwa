a8a2b2dc1f2cf9aacc7d1ddf8e1ef29fd740ded8
Simon Grimm <saimon@devdactic.com>
2020-11-30 10:52:40 +0100

    Add PWA functionality.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  users = [];
+  joke = null;
+  
+  appIsOnline = true;
+  browserOffline: Subscription;
+  browserOnline: Subscription;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.browserOffline = fromEvent(window, 'offline').subscribe(() => {
+      this.appIsOnline = false;
+    });
+
+    this.browserOnline = fromEvent(window, 'online').subscribe(() => {
+      this.appIsOnline = true;
+    });
+  }
+
+  getData() {
+    this.http.get('https://randomuser.me/api/?results=5').subscribe(result => {
+      console.log('results: ', result);
+      this.users = result['results'];
+    });
+  }
+
+  getOnlineData() {
+    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(result => {
+      console.log('joke result: ', result);
+      this.joke = result;
+    });
+  }
 
 }
